454675dc2609628118e3ba3e605a09308e4d8b5a
Brycen Bartolome <brycenbartolome@gmail.com>
2020-12-11 10:13:09 -0800

    add firebase to project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import 'firebase/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID 
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firesstore();
+
 function App() {
   return (
     
